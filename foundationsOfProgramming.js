15f2a92510955fb34fc651c65e963206e2c22521
Quarps <landerzen10@gmail.com>
2023-02-08 08:21:37 +0100

    All chapters finished

--- foundationsOfProgramming.js
+++ foundationsOfProgramming.js
@@ -393,9 +393,231 @@
 // }
 
 //14: WHILE LOOPS
-let count = 0;
+// let count = 0;
 
-while (count < 5) {
-  console.log(count);
-  count += 1;
-}
+// while (count <= 14) {
+//   ellipse(30 + count * 50, 100, 40);
+//   count += 1;
+// }
+
+//14: WHILE LOOPS EXERCISE - SQUARED PAPER
+// background(255, 255, 255);
+// let x = 0;
+// let y = 0;
+// const size = 80;
+
+// while (x < width) {
+//   line(x, 0, x, height);
+//   x += size;
+// }
+
+// while (y < height) {
+//   line(0, y, width, y);
+//   x += size;
+// }
+
+//14: FOR LOOPS
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+//14: FOR LOOPS EXERCISE - DRAW 5 RECTANGALES
+
+// background(255, 255, 255);
+
+// let size = 50;
+// let margin = 50;
+// let x = margin;
+
+// for (let i = 0; i < 6; i++) {
+//   const percentage = (i + 1) / (5 - 2);
+//   rect(x, 100 - (size / 2) * percentage, size * percentage, size * percentage);
+//   x = x + size * percentage + margin;
+// }
+
+//14: FOR LOOPS EXERCISE - DRAW A CHECKERBOARD - NESTED FOR LOOP
+// const size = 70;
+// const length = 10;
+
+// background(125, 125, 125);
+// // noStroke();
+
+// for (let x = 0; x < length; x++) {
+//   for (let y = 0; y < length; y++) {
+//     if (y % 2 === 0) {
+//       if (x % 2 === 0) {
+//         fill(0, 0, 0);
+//       } else {
+//         fill(255, 255, 255);
+//       }
+//     } else {
+//       if (x % 2 === 0) {
+//         fill(255, 255, 255);
+//       } else {
+//         fill(0, 0, 0);
+//       }
+//     }
+//     rect(x * size, y * size, size, size);
+//   }
+// }
+
+//15: ARRAYS
+
+//array[index] (a position in the list = index)
+//array.length (counts the amount of objects in the list)
+//arrray.indexOf("...") (generates the index of an object - position in the list)
+//array.join("...") (lets all element join together on a single row, as a string)
+//array.push("...") (adds the element to the end of the list, dont mix strings and numbers)
+//array.pop("...") (removes an element, on the end of the list)
+
+//array.unshift("...") (adds an element in the beginning)
+//array.shift("...") (removes an element in the beginning)
+//array.splice("position", "amount to remove", "add elements", "add elements", ...)
+
+// let animals = ["d1og", "bnny", "cat", "monke", "lion"];
+
+// console.log(animals);
+
+// function draw() {
+//   background(255, 255, 255);
+//   text(animals.join(" "), 20, 100);
+// }
+
+// function mouseClicked() {
+//   animals.push("added");
+// }
+
+//15: ARRAYS AND LOOPS
+
+// let animals = ["🐶", "🐰", "🐱", "🐵", "🦁"];
+
+// function draw() {
+//   background(255, 255, 255);
+//   textSize(60);
+//   for (let index in animals) {
+//     let animal = animals[index];
+//     text(animal, 20, 70 + 70 * index);
+//   }
+// }
+
+//ANOTHER WAY
+
+// let animals = ["🐶", "🐰", "🐱", "🐵", "🦁"];
+
+// function draw() {
+//   background(255, 255, 255);
+//   textSize(60);
+//   let y = 70;
+//   for (let animal of animals) {
+//     text(animal, 20, y);
+//     y += 70;
+//   }
+// }
+
+//15: ARRAYS AND RETURN VALUES IN FUNCTIONS
+// function randomElement(array) {
+//   // let randomIndex = Math.random() * array.length;
+//   // randomIndex = Math.floor(randomIndex);
+//   // return array[randomIndex];
+
+//   return array[Math.floor(Math.random() * array.length)];
+// }
+
+// const animals = ["🐶", "🐰", "🐱", "🐵", "🦁", "🐤"];
+
+// function mouseClicked() {
+//   background(255, 255, 255);
+//   textSize(60);
+//   const randomAnimal = randomElement(animals);
+//   text(randomAnimal, 20, 100);
+// }
+
+//15: EXERCISE - REVERSE THE ORDER
+// function reverse(array) {
+//   let reversedArray = [];
+
+//   for (let element of array) {
+//     reversedArray.unshift(element);
+//   }
+
+//   return reversedArray;
+// }
+// const exampleArray = ["🐶", "🐰", "🐱", "🐵", "🦁", "🐤"];
+// console.log(reverse(exampleArray));
+
+//15: EXERCISE - RANDOMZIE
+
+// function randomize(array) {
+//   let randomizedArray = [];
+
+//   while (array.length > 0) {
+//     const randomIndex = Math.floor(Math.random() * array.length);
+//     randomizedArray.push(array[randomIndex]);
+//     array.splice(randomIndex, 1);
+//   }
+//   return randomizedArray;
+// }
+
+// const exampleArray = ["🐶", "🐰", "🐱", "🐵", "🦁", "🐤"];
+// console.log(randomize(exampleArray));
+
+// 15: EXERCISE - REMEMBER THE LAST 5 KEYS
+
+// let lastKeys = [];
+
+// function draw() {
+//   background(255, 255, 255);
+//   fill(0, 0, 0);
+//   textSize(60);
+//   text(lastKeys.join(", "), 40, 100);
+// }
+
+// function keyReleased() {
+//   lastKeys.unshift(key);
+//   if (lastKeys.length > 5) {
+//     lastKeys.pop;
+//   }
+// }
+
+//15: EXAMPLE - NIGHT SKY
+// let starX = [];
+// let starY = [];
+// let starAlpha = [];
+
+// for (let i = 0; i < 500; i++) {
+//   const x = Math.floor(Math.random() * width);
+//   const y = Math.floor(Math.random() * height);
+//   const alpha = Math.random();
+
+//   starX.push(x);
+//   starY.push(y);
+//   starAlpha.push(alpha);
+// }
+
+// function draw() {
+//   noStroke();
+//   background(0, 0, 0);
+
+//   for (let index in starX) {
+//     fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+//     ellipse(starX[index], starY[index], 3);
+//     starAlpha[index] += 0.02;
+//   }
+// }
+
+//16: SIMPLE OJBECTS
+// let poistion = { x: 100, y: 100 };
+
+// let user = {
+//   name: "Ludwig",
+//   role: "Student",
+//   school: "Jönköping University",
+// };
+
+// console.log(user.school);
+
+// function draw() {
+//   ellipse(poistion.x, poistion.y, 40);
+//   poistion.x += 1;
+//   poistion.y += 2;
+// }
